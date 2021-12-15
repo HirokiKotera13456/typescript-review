d11598f8574909cd3371c16b6f4f415d9fed7359
nijimajohn <86758487+nijimajohn@users.noreply.github.com>
2021-12-15 13:29:05 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+let bool: boolean = true;
+
+let num: number = 0;
+
+let str: string = "A";
+
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+let any1: any = false;
